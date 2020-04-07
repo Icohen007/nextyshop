17394de918d52b75717ae9b9cc85654bdf3e83ab
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-07 16:42:05 +0300

    * get product by _id with findOne.
    in product page:
    - use 'query' of ctx.
    - axios.get with payload.

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,15 @@
-function Product() {
+import axios from 'axios';
+
+function Product({ product }) {
+  console.log({ product });
   return <>product</>;
 }
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+  const url = 'http://localhost:3000/api/website';
+  const payload = { params: { _id } }; // same as concating ?_id=
+  const response = await axios.get(url, payload);
+  return { product: response.data };
+};
 
 export default Product;
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -4,20 +4,20 @@ const connectionStatus = { isConnected: false };
 
 
 const mongooseDeprecatedConfig = {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
 };
 
 async function dbConnection() {
-    if (connectionStatus.isConnected) {
-        console.log('Using existing connection');
-        return;
-    }
-    const db = await mongoose.connect(process.env.MONGO_SRV, mongooseDeprecatedConfig);
-    console.log('MongoDB database connection established successfully');
-    connectionStatus.isConnected = db.connections[0].readyState;
+  if (connectionStatus.isConnected) {
+    console.log('Using existing connection');
+    return;
+  }
+  const db = await mongoose.connect(process.env.MONGO_SRV, mongooseDeprecatedConfig);
+  console.log('MongoDB database connection established successfully');
+  connectionStatus.isConnected = db.connections[0].readyState;
 }
 
 export default dbConnection;
