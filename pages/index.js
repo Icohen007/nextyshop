c4c10222e2aa1134ed963508efe81bff715243df
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-07 15:56:23 +0300

    * add eslint * add axios * fetch products from json with getInitialProps of NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-function Home() {
+import React from 'react';
+import axios from 'axios';
+
+const getProducts = async () => {
+    const url = 'http://localhost:3000/api/products';
+    const products = await axios.get(url);
+    return { products: products.data };
+};
+
+function Home({ websites: products }) {
+    console.log(products);
     return <>home</>;
 }
+
+Home.getInitialProps = getProducts;
 
 export default Home;
