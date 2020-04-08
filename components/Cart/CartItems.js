3a04fc1b20e125e6e3cf62b45319e992613bb5de
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-08 15:36:07 +0300

    Basic cart structure.

---
+++ components/Cart/CartItems.js
@@ -0,0 +1,25 @@
+import {
+  Header, Segment, Button, Icon,
+} from 'semantic-ui-react';
+
+function CartItems() {
+  const user = false;
+
+  return (
+    <Segment secondary color="violet" inverted textAlign="center" placeholder>
+      <Header icon>
+        <Icon name="shopping basket" />
+        No products in your cart. Add some!
+      </Header>
+      <div>
+        {user ? (
+          <Button color="orange">View Products</Button>
+        ) : (
+          <Button color="blue">Login to Add Products</Button>
+        )}
+      </div>
+    </Segment>
+  );
+}
+
+export default CartItems;
--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,14 @@
+import { Segment } from 'semantic-ui-react';
+import CartItems from '../components/Cart/CartItems';
+import CartSummary from '../components/Cart/CartSummary';
+
 function Cart() {
-  return <>cart</>;
+  return (
+    <Segment>
+      <CartItems />
+      <CartSummary />
+    </Segment>
+  );
 }
 
 export default Cart;
