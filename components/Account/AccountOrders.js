18b794411fa5c8dae5f806e9583ca3a788009278
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-13 01:23:25 +0300

    Add orders api endpoint
    in account page: add network request to orders api.
    
    Add Account Orders Component.

---
+++ components/Account/AccountOrders.js
@@ -0,0 +1,88 @@
+import {
+  Header, Accordion, Label, Segment, Icon, Button, List, Image, Container,
+} from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+
+const OrderItem = ({ product, quantity }) => (
+  <List.Item>
+    <Image avatar src={product.mediaUrl} />
+    <List.Content>
+      <List.Header>{product.name}</List.Header>
+      <List.Description>
+        {`${quantity} x $${product.price}`}
+      </List.Description>
+    </List.Content>
+    <List.Content floated="right">
+      <Label tag color="red" size="tiny">
+        {product.sku}
+      </Label>
+    </List.Content>
+  </List.Item>
+);
+
+function mapOrdersToPanels(orders) {
+  return orders.map(({
+    _id, createdAt, email, products, total,
+  }) => ({
+    key: _id,
+    title: {
+      content: <Label color="blue" content={createdAt} />,
+    },
+    content: {
+      content: (
+        <Container>
+          <List.Header as="h3">
+            {`Total: $${total}`}
+            <Label
+              content={email}
+              icon="mail"
+              basic
+              horizontal
+              style={{ marginLeft: '1em' }}
+            />
+          </List.Header>
+          <List>
+            {products.map(({ product, quantity }) => (
+              <OrderItem product={product} quantity={quantity} />
+            ))}
+          </List>
+        </Container>
+      ),
+    },
+  }));
+}
+
+function AccountOrders({ orders }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header as="h2">
+        <Icon name="folder open" />
+        Order History
+      </Header>
+      {orders.length ? (
+        <Accordion
+          fluid
+          styled
+          exclusive={false}
+          panels={mapOrdersToPanels(orders)}
+        />
+      ) : (
+        <Segment inverted tertiary color="grey" textAlign="center">
+          <Header icon>
+            <Icon name="copy outline" />
+            No past orders.
+          </Header>
+          <div>
+            <Button onClick={() => router.push('/')} color="orange">
+              View Products
+            </Button>
+          </div>
+        </Segment>
+      )}
+    </>
+  );
+}
+
+export default AccountOrders;
---
+++ pages/api/orders.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import Order from '../../models/Order';
+import dbConnection from '../../utils/dbConnection';
+
+dbConnection();
+
+export default async (req, res) => {
+  const { authorization } = req.headers;
+  try {
+    const { userId } = jwt.verify(authorization, process.env.JWT_SECRET);
+    const orders = await Order.find({ user: userId })
+      .populate({
+        path: 'products.product',
+        model: 'Product',
+      });
+    res.status(200).json({ orders });
+  } catch (error) {
+    console.error(error);
+    res.status(403).send('Please login again');
+  }
+};
