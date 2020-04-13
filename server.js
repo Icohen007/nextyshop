72320b69af98f6dc86b489b3b6792f050c119f64
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-14 01:10:21 +0300

    remove customConfig file

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const next = require('next');
 const cors = require('cors');
 const dbConnection = require('./utils/dbConnection');
-const customConfig = require('./next.config.js');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, conf: { ...customConfig } });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
